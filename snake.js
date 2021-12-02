84a0737c8e9d169047f6586e3dca7070c2b53240
huyp3 <huyp3@fpt.com.vn>
2021-12-02 22:46:27 +0700

    add movement and refactor

--- snake.js
+++ snake.js
@@ -1,43 +1,49 @@
 const Cell = require("./cell");
 const { GameOverError } = require("./errors");
 const _find = require("lodash/find");
+const _head = require("lodash/head");
+const _tail = require("lodash/tail");
+
 class Snake {
   constructor() {
     this.cells = [new Cell(9, 9)];
   }
-  goEast = () => {
-    if (this.cells.length == 1) {
-      const curHead = this.cells[0];
-      if (curHead.col == 19) {
-        throw new GameOverError();
-      }
-      this.cells = [new Cell(++curHead.col, curHead.row)];
-    } else {
-      const curHead = this.cells[this.cells.length - 1];
-      this.cells.push(new Cell(++curHead.col, curHead.row));
-      this.cells.shift();
+  getHead() {
+    return this.cells[this.cells.length - 1];
+  }
+  checkWall(head) {
+    if (head.col < 0 || head.col > 19 || head.row < 0 || head.row > 19) {
+      throw new GameOverError();
     }
+  }
+  makeHead(direct) {
+    const curHead = this.getHead();
+    if (direct == "east") {
+      return new Cell(curHead.col + 1, curHead.row);
+    } else if (direct == "west") {
+      return new Cell(curHead.col - 1, curHead.row);
+    } else if (direct == "north") {
+      return new Cell(curHead.col, curHead.row - 1);
+    } else if (direct == "south") {
+      return new Cell(curHead.col, curHead.row + 1);
+    }
+  }
+  move(direction) {
+    const newHead = this.makeHead(direction);
+    this.checkWall(newHead);
+    this.cells = [...this.cells.slice(1), newHead];
+  }
+  goEast = () => {
+    this.move("east");
   };
   goWest = () => {
-    const head = this.cells[0];
-    if (head.col == 0) {
-      throw new GameOverError();
-    }
-    this.cells = [new Cell(--head.col, head.row)];
+    this.move("west");
   };
   goNorth = () => {
-    const head = this.cells[0];
-    if (head.row == 0) {
-      throw new GameOverError();
-    }
-    this.cells = [new Cell(head.col, --head.row)];
+    this.move("north");
   };
   goSouth = () => {
-    const head = this.cells[0];
-    if (head.row == 19) {
-      throw new GameOverError();
-    }
-    this.cells = [new Cell(head.col, ++head.row)];
+    this.move("south");
   };
   addNode = (cell) => {
     this.cells.push(cell);
